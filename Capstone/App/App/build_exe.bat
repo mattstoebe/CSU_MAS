3d32456a7c8090373426a4e1d5ac43ca86974d42
Matt <stoebematt@gmail.com>
2025-06-06 16:59:00 -0500

    finish model training

--- Capstone/App/App/build_exe.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Run from project root. Generates /dist/app.exe
-pyinstaller ^
-    --onefile ^
-    --add-data "models/model.joblib;models" ^
-    --add-data "assets/example_input.xlsx;assets" ^
-    --collect-submodules "sklearn" ^
-    src/app.py
-pause
